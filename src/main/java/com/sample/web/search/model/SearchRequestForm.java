b37ba27bfb3905aa9925bc35f65c450bdfda55e3
twan65 <cx.twan65@gmail.com>
2020-12-05 19:14:06 +0900

    message.propertiesの作成

--- src/main/java/com/sample/web/search/model/SearchRequestForm.java
+++ src/main/java/com/sample/web/search/model/SearchRequestForm.java
@@ -14,31 +14,31 @@ import java.util.List;
 public class SearchRequestForm {
 
     // タイトル
-    @Max(value = 30, message = "30文字以下で入力してください")
+    @Max(value = 30, message = "{VE00002}")
     private String title;
 
     // 概要
-    @Max(value = 300, message = "300文字以下で入力してください")
+    @Max(value = 300, message = "{VE00002}")
     private String overview;
 
     // 掲載開始日FROM
-    @DateFormatValid
+    @DateFormatValid(message = "{VE00003}")
     private String displayStartDateFrom;
 
     // 掲載開始日TO
-    @DateFormatValid
+    @DateFormatValid(message = "{VE00003}")
     private String displayStartDateTo;
 
     // 掲載終了日FROM
-    @DateFormatValid
+    @DateFormatValid(message = "{VE00003}")
     private String displayEndDateFrom;
 
     // 掲載終了日TO
-    @DateFormatValid
+    @DateFormatValid(message = "{VE00003}")
     private String displayEndDateTo;
 
     // お知らせタイプリスト
-    private List<@Min(value = 1, message = "入力したコードは存在しません") @Max(value = 4, message = "入力したコードは存在しません") Integer> informationTypeList;
+    private List<@Min(value = 1, message = "{VE00006}") @Max(value = 4, message = "{VE00006}") Integer> informationTypeList;
 
 
     /**
