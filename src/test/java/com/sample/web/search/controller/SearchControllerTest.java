5962f9964e559597b1883355d7d8dcad7581e5a4
twan65 <cx.twan65@gmail.com>
2020-12-07 20:59:01 +0900

    お知らせ一覧のテストコード追加

--- src/test/java/com/sample/web/search/controller/SearchControllerTest.java
+++ src/test/java/com/sample/web/search/controller/SearchControllerTest.java
@@ -19,6 +19,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.servlet.ModelAndView;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -35,6 +38,21 @@ public class SearchControllerTest {
 
     private final static String FORM_NAME_SEARCH = "searchRequestForm";
     private final static String URL_SEARCH = "/search";
+    private final static List<String> NG_DATE_FORMATS = new ArrayList<>(){
+        {
+            add("2020/12/12");
+            add("2020.12.12");
+            add("2020-12-12");
+        }
+    };
+
+    private final static List<Integer> NG_INFORMATION_TYPES = new ArrayList<>(){
+        {
+            add(-1);
+            add(0);
+            add(5);
+        }
+    };
 
     @Before
     public void init() {
@@ -65,6 +83,126 @@ public class SearchControllerTest {
         ModelAndView mnv = actions.andReturn().getModelAndView();
         BindingResult bindingResult = (BindingResult) mnv.getModel().get(BindingResult.MODEL_KEY_PREFIX + FORM_NAME_SEARCH);
 
-        assertThat("{VE00002}").isEqualTo(bindingResult.getFieldError().getDefaultMessage());
+        assertThat(bindingResult.getFieldError().getDefaultMessage()).isEqualTo("{VE00002}");
+    }
+
+    @Test
+    @DisplayName("概要が300文字超えた場合")
+    public void validateOverviewOver300CharactersTest() throws Exception {
+        SearchRequestForm form = new SearchRequestForm();
+        form.setOverview("あいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえお" +
+                "あいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえお" +
+                "あいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえお" +
+                "あいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえお" +
+                "あいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえお" +
+                "あいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえお" +
+                "あいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえお");
+
+        ResultActions actions = mockMvc.perform(get(URL_SEARCH).flashAttr(FORM_NAME_SEARCH, form))
+                .andExpect(model().hasErrors())
+                .andExpect(model().errorCount(1));
+
+        ModelAndView mnv = actions.andReturn().getModelAndView();
+        BindingResult bindingResult = (BindingResult) mnv.getModel().get(BindingResult.MODEL_KEY_PREFIX + FORM_NAME_SEARCH);
+
+        assertThat(bindingResult.getFieldError().getDefaultMessage()).isEqualTo("{VE00002}");
+    }
+
+    @Test
+    @DisplayName("掲載開始日Fromのフォーマットが「yyyy年MM月dd日」ではない場合")
+    public void validateDisplayStartDateFromFormatTest() throws Exception {
+
+        for (String format : NG_DATE_FORMATS) {
+            SearchRequestForm form = new SearchRequestForm();
+            form.setDisplayStartDateFrom(format);
+
+            ResultActions actions = mockMvc.perform(get(URL_SEARCH).flashAttr(FORM_NAME_SEARCH, form))
+                    .andExpect(model().hasErrors())
+                    .andExpect(model().errorCount(1));
+
+            ModelAndView mnv = actions.andReturn().getModelAndView();
+            BindingResult bindingResult = (BindingResult) mnv.getModel().get(BindingResult.MODEL_KEY_PREFIX + FORM_NAME_SEARCH);
+
+            assertThat(bindingResult.getFieldError().getDefaultMessage()).isEqualTo("{VE00003}");
+        }
+
+    }
+
+    @Test
+    @DisplayName("掲載開始日Toのフォーマットが「yyyy年MM月dd日」ではない場合")
+    public void validateDisplayStartDateToFormatTest() throws Exception {
+
+        for (String format : NG_DATE_FORMATS) {
+            SearchRequestForm form = new SearchRequestForm();
+            form.setDisplayStartDateTo(format);
+
+            ResultActions actions = mockMvc.perform(get(URL_SEARCH).flashAttr(FORM_NAME_SEARCH, form))
+                    .andExpect(model().hasErrors())
+                    .andExpect(model().errorCount(1));
+
+            ModelAndView mnv = actions.andReturn().getModelAndView();
+            BindingResult bindingResult = (BindingResult) mnv.getModel().get(BindingResult.MODEL_KEY_PREFIX + FORM_NAME_SEARCH);
+
+            assertThat(bindingResult.getFieldError().getDefaultMessage()).isEqualTo("{VE00003}");
+        }
+    }
+
+    @Test
+    @DisplayName("掲載終了日Fromのフォーマットが「yyyy年MM月dd日」ではない場合")
+    public void validateDisplayEndDateFromFormatTest() throws Exception {
+
+        for (String format : NG_DATE_FORMATS) {
+            SearchRequestForm form = new SearchRequestForm();
+            form.setDisplayEndDateFrom(format);
+
+            ResultActions actions = mockMvc.perform(get(URL_SEARCH).flashAttr(FORM_NAME_SEARCH, form))
+                    .andExpect(model().hasErrors())
+                    .andExpect(model().errorCount(1));
+
+            ModelAndView mnv = actions.andReturn().getModelAndView();
+            BindingResult bindingResult = (BindingResult) mnv.getModel().get(BindingResult.MODEL_KEY_PREFIX + FORM_NAME_SEARCH);
+
+            assertThat(bindingResult.getFieldError().getDefaultMessage()).isEqualTo("{VE00003}");
+        }
+    }
+
+    @Test
+    @DisplayName("掲載終了日Toのフォーマットが「yyyy年MM月dd日」ではない場合")
+    public void validateDisplayEndDateToFormatTest() throws Exception {
+
+        for (String format : NG_DATE_FORMATS) {
+            SearchRequestForm form = new SearchRequestForm();
+            form.setDisplayEndDateTo(format);
+
+            ResultActions actions = mockMvc.perform(get(URL_SEARCH).flashAttr(FORM_NAME_SEARCH, form))
+                    .andExpect(model().hasErrors())
+                    .andExpect(model().errorCount(1));
+
+            ModelAndView mnv = actions.andReturn().getModelAndView();
+            BindingResult bindingResult = (BindingResult) mnv.getModel().get(BindingResult.MODEL_KEY_PREFIX + FORM_NAME_SEARCH);
+
+            assertThat(bindingResult.getFieldError().getDefaultMessage()).isEqualTo("{VE00003}");
+        }
+    }
+
+    @Test
+    @DisplayName("お知らせタイプコードが範囲外の場合")
+    public void validateInformationTypeListTest() throws Exception {
+
+        for (Integer type : NG_INFORMATION_TYPES) {
+            SearchRequestForm form = new SearchRequestForm();
+            List<Integer> informationTypeList = new ArrayList<>();
+            informationTypeList.add(type);
+            form.setInformationTypeList(informationTypeList);
+
+            ResultActions actions = mockMvc.perform(get(URL_SEARCH).flashAttr(FORM_NAME_SEARCH, form))
+                    .andExpect(model().hasErrors())
+                    .andExpect(model().errorCount(1));
+
+            ModelAndView mnv = actions.andReturn().getModelAndView();
+            BindingResult bindingResult = (BindingResult) mnv.getModel().get(BindingResult.MODEL_KEY_PREFIX + FORM_NAME_SEARCH);
+
+            assertThat(bindingResult.getFieldError().getDefaultMessage()).isEqualTo("{VE00006}");
+        }
     }
 }
